0310a6d5def8b3bceae29956fd13ed012aa6f9d0
Lucas Q <lucas.queveau44190@gmail.com>
2026-01-06 11:04:43 +0100

    chore : add pages and routes

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import Adoption from "./pages/Adoption.tsx";
 import { BrowserRouter } from "react-router";
 import { Route, Routes } from "react-router";
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/Adoption" element={<Adoption />} />
     </Routes>
   </BrowserRouter>
 );
