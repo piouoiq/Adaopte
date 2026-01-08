66b7e4aff5ec9ca7639a3d07fc66ae0b86144d1e
piouoiq <melargilliere@gmail.com>
2026-01-08 12:07:48 +0100

    feat : main tittle

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,21 @@
 export default function App() {
   return (
     <>
-      <section className=" text-center text-primary-color py-40 bg-[url(../public/images/background.jpg)] bg-cover bg-center brightness-80 font-family-heading">
-        <h1 className="text-6xl pb-12">
-          Donnons-leurs autant qu'ils nous apportent
-        </h1>
-        <p>
-          Chaque jour, des milliers d'animaux attendent une famille aimante.
-          Trouvez votre <br />
-          compagnon idéal parmi nos animaux disponibles à l'adoption.
-        </p>
+      <section className=" relative overflow-hidden text-center text-primary-color py-40 font-family-heading">
+        <div className=" relative z-50 ">
+          <h1 className="text-6xl pb-12">
+            Donnons-leurs autant qu'ils nous apportent
+          </h1>
+          <p>
+            Chaque jour, des milliers d'animaux attendent une famille aimante.
+            Trouvez votre <br />
+            compagnon idéal parmi nos animaux disponibles à l'adoption.
+          </p>
+        </div>
+        <img
+          src="../public/images/background.jpg"
+          className=" w-full h-full object-cover object-center brightness-80  absolute top-0"
+        ></img>
       </section>
     </>
   );
