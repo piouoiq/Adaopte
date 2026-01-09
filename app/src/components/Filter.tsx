122335288599b35fd304d6eb7a98847c33fc3f62
Lucas Q <lucas.queveau44190@gmail.com>
2026-01-09 13:27:02 +0100

    chore add all choice to filter

--- app/src/components/Filter.tsx
+++ app/src/components/Filter.tsx
@@ -1,14 +1,20 @@
-export function Filter(){
-    return(
-        <div>
-            <div>
-                <h3>Type d'animal</h3>
-                <select></select>
-            </div>
-            <div>
-                <h3>Localisation</h3>
-                <input type="search" placeholder="Votre ville" />
-            </div>
-        </div>
-    )
+import { type } from "../data/animals";
+
+export function Filter() {
+  return (
+    <div>
+      <div>
+        <h3>Type d'animal</h3>
+        <select>
+          {type.map((key) => (
+            <option key={key.type}>{key.type}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <h3>Localisation</h3>
+        <input type="search" placeholder="Votre ville" />
+      </div>
+    </div>
+  );
 }
