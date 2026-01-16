1715db42cdee1d4a90ef3f502f533fd5593b7300
piouoiq <melargilliere@gmail.com>
2026-01-16 10:35:49 +0100

    feat composant header

---
+++ app/src/components/pageLinks.tsx
@@ -0,0 +1,8 @@
+import { LuHouse, LuBone, LuFish, LuSquirrel } from "react-icons/lu";
+
+export const link = [
+  { link: "/", label: "Accueil", icon: <LuHouse /> },
+  { link: "/Adoption", label: "J'adopte", icon: <LuBone /> },
+  { link: "/", label: "Guide de l'adoption", icon: <LuFish /> },
+  { link: "/Volunteer", label: "Devenir bénévole", icon: <LuSquirrel /> },
+];
